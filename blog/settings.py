20b9a60a845e7f3bd410e08e4d8c30e4abf208c0
antony_kosenko <kosenko.anton.work@gmail.com>
2024-05-13 13:00:04 +0300

    fixing asgi host allowance

--- blog/settings.py
+++ blog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = ["*"]
 INTERNAL_IPS = ["*"]
