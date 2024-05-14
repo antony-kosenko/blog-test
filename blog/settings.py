f12616ad30b920aa5b7d5787940a671dd2c9f566
antony_kosenko <kosenko.anton.work@gmail.com>
2024-05-14 14:33:09 +0300

    switched db for docker

--- blog/settings.py
+++ blog/settings.py
@@ -182,22 +182,22 @@ if DEBUG:
     }
 else:
 
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.mysql',
-    #         'NAME': 'db_django',
-    #         'PASSWORD': 'password',
-    #         'HOST': 'my_db',
-    #         'PORT': 3306,
-    #     }
-    # }
-
     DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default=os.environ.get("DB_URL"),
-        conn_max_age=600
-    )
-}
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'db_django',
+            'PASSWORD': 'password',
+            'HOST': 'my_db',
+            'PORT': 3306,
+        }
+    }
+
+#     DATABASES = {
+#     'default': dj_database_url.config(
+#         # Replace this value with your local database's connection string.
+#         default=os.environ.get("DB_URL"),
+#         conn_max_age=600
+#     )
+# }
     STATIC_ROOT = BASE_DIR / 'staticfiles'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
