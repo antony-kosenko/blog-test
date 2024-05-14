2e865a2572f271633aed22af9b7e17bc425799d3
antony_kosenko <kosenko.anton.work@gmail.com>
2024-05-14 14:36:47 +0300

    settings fix

--- blog/settings.py
+++ blog/settings.py
@@ -198,5 +198,5 @@ else:
         conn_max_age=600
     )
 }
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
